2148164f8fa5e1ba51a054f7e10dec309658fcb6
Melvin Bubini <melvin.bubini@gmail.com>
2024-11-14 15:48:00 +0100

    klar med ramverk 1

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+"use strict"
+
 window.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 1) {
@@ -10,4 +12,32 @@ window.addEventListener("scroll", function () {
 document.getElementById('scroll-btn').addEventListener('click', function (event) {
     event.preventDefault();
     document.querySelector('#answers-section').scrollIntoView({behavior: 'smooth'});
+});
+
+document.getElementById("userForm").addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Ta upp värden som lagts in i formuläret
+    const firstname = document.getElementById('firstname').value;
+    const age = document.getElementById('age').value;
+    const framework = document.getElementById('frameworkSelect').value;
+    const textarea = document.getElementById('floatingTextarea').value;
+
+    const userTable = document.getElementById('userTable').querySelector('tbody');
+    const newRow = document.createElement('tr');
+
+    // Lägg till datan i formuläret
+    newRow.innerHTML = `
+        <td>${userTable.rows.length + 1}</td>
+        <td>${firstname}</td>
+        <td>${age}</td>
+        <td>${framework}</td>
+        <td>${textarea}</td>
+    `;
+
+    // Append nya raden till tabellen
+    userTable.appendChild(newRow);
+
+    // Rensar formuläret
+    event.target.reset();
 });
