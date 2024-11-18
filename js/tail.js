28d39e894dcb9dcf35cd70f83e4c002d3a3716a1
Melvin Bubini <melvin.bubini@gmail.com>
2024-11-18 11:38:17 +0100

    svar på frågorna kvar

---
+++ js/tail.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// Toggle Sidebar for Mobile
+const menuButton = document.getElementById("menu-button");
+const sidebar = document.getElementById("sidebar");
+const closeSidebar = document.getElementById("close-sidebar");
+
+menuButton.addEventListener("click", () => {
+    sidebar.classList.toggle("translate-x-full");
+});
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.classList.add("translate-x-full");
+});
+
+// Make Navbar Transparent on Scroll
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 1) {
+        navbar.classList.remove("bg-opacity-100"); // Remove background
+        navbar.classList.add("bg-opacity-0");      // Make transparent
+    } else {
+        navbar.classList.add("bg-opacity-100");     // Add background
+        navbar.classList.remove("bg-opacity-0");    // Remove transparency
+    }
+});
+
+document.getElementById("tailwindUserForm").addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Ta upp värden som lagts in i formuläret
+    const firstname = document.getElementById('firstname').value;
+    const age = document.getElementById('age').value;
+    const framework = document.getElementById('frameworkSelect').value;
+    const textarea = document.getElementById('floatingTextarea').value;
+
+    const userTable = document.getElementById('tailwindUserTable').querySelector('tbody');
+    const newRow = document.createElement('tr');
+
+    // Lägg till klassen hover:bg-gray-700
+    newRow.classList.add('hover:bg-gray-700');
+
+    // Lägg till datan i formuläret
+    newRow.innerHTML = `
+        <td class="py-2 px-4 border-b border-gray-700 text-gray-300">${userTable.rows.length + 1}</td>
+        <td class="py-2 px-4 border-b border-gray-700 text-gray-300">${firstname}</td>
+        <td class="py-2 px-4 border-b border-gray-700 text-gray-300">${age}</td>
+        <td class="py-2 px-4 border-b border-gray-700 text-gray-300">${framework}</td>
+        <td class="py-2 px-4 border-b border-gray-700 text-gray-300">${textarea}</td>
+    `;
+
+    // Append nya raden till tabellen
+    userTable.appendChild(newRow);
+
+    // Rensar formuläret
+    event.target.reset();
+});
